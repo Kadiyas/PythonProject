9975aee0648126660045819833edceba65ca7215
kadiyas <1228164670@qq.com>
2018-08-17 17:54:04 +0800

    lesson001 数据类型 lesson002 字符串编码

--- work/lessons/lesson003.py
+++ work/lessons/lesson003.py
@@ -2,158 +2,181 @@
 # -*- coding: utf-8 -*-
 import os
 
-# 高级特性
-print('--------------切片(Slice)---------------')
-# list tuple 字符串 都可以切片
-L = ['L0', 'L1', 'L2', 'L3', 'L4', 'L5', 'L6', 'L7', 'L8', 'L9', 'L10', 'L11']
-print("L == ", L)
-print('L 取0~2 = ', L[0:3])
-print('L 取倒数第二个~最后一个 = ', L[-2:])
-print('L 前10个数，每两个取一个： = ', L[0:10:2])
-print('L 所有数，每5个取一个：： = ', L[::5])
+# 使用list和tuple
+# 阅读: 885726
+# ---------------------list----------------------
+# Python内置的一种数据类型是列表：list。list是一种有序的集合，可以随时添加和删除其中的元素。
+#
+# 比如，列出班里所有同学的名字，就可以用一个list表示：
+classmates = ['李明', '王三丰', '炸天']
+print('classmates --> ', classmates)
+print('len(classmates)  -->', len(classmates))
+# >>> classmates = ['Michael', 'Bob', 'Tracy']
+# >>> classmates
+# ['Michael', 'Bob', 'Tracy']
+# 变量classmates就是一个list。用len()函数可以获得list元素的个数：
+#
+# >>> len(classmates)
+# 3
+# 用索引来访问list中每一个位置的元素，记得索引是从0开始的：
+print("classmates[0] --> ", classmates[0])
+# >>> classmates[0]
+# 'Michael'
+# >>> classmates[1]
+# 'Bob'
+# >>> classmates[2]
+# 'Tracy'
+# >>> classmates[3]
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# IndexError: list index out of range
+# 当索引超出了范围时，Python会报一个IndexError错误，所以，要确保索引不要越界，记得最后一个元素的索引是len(classmates) - 1。
+#
+# 如果要取最后一个元素，除了计算索引位置外，还可以用-1做索引，直接获取最后一个元素：
+print('classmates[-1] --> ', classmates[-1])
+# >>> classmates[-1]
+# 'Tracy'
+# 以此类推，可以获取倒数第2个、倒数第3个：
+#
+# >>> classmates[-2]
+# 'Bob'
+# >>> classmates[-3]
+# 'Michael'
+# >>> classmates[-4]
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# IndexError: list index out of range
+# 当然，倒数第4个就越界了。
 
-# 什么都不写，只写[:]就可以原样复制一个list：L[:]
-L1 = L[:]
-print('L1 = ', L1)
+# list是一个可变的有序表，所以，可以往list中追加元素到末尾：
+classmates.append('尼古拉斯')
+print('classmates after classmates.append("尼古拉斯) --> ', classmates)
+# >>> classmates.append('Adam')
+# >>> classmates
+# ['Michael', 'Bob', 'Tracy', 'Adam']
 
-# tuple也是一种list，所以也可以切片
-print('tuple也可以切片', (0, 1, 2, 3, 4, 5, 6, 7)[:3])
+# 也可以把元素插入到指定的位置，比如索引号为1的位置：
+classmates.insert(1, '赵四')
+print('classmates after classmates.insert(1, "赵四") --> ', classmates)
+# >>> classmates.insert(1, 'Jack')
+# >>> classmates
+# ['Michael', 'Jack', 'Bob', 'Tracy', 'Adam']
 
-
+# 要删除list末尾的元素，用pop()方法：
+classmates.pop()
+print('classmates after classmates.pop() -- > ', classmates)
+classmates.pop(1)
+print('classmates after classmates.pop(1) -- > ', classmates)
+# >>> classmates.pop()
+# 'Adam'
+# >>> classmates
+# ['Michael', 'Jack', 'Bob', 'Tracy']
+# 要删除指定位置的元素，用pop(i)方法，其中i是索引位置：
+#
+# >>> classmates.pop(1)
+# 'Jack'
+# >>> classmates
+# ['Michael', 'Bob', 'Tracy']
+# 要把某个元素替换成别的元素，可以直接赋值给对应的索引位置：
+classmates[2] = 23.3
+print('classmates after set classmates[2] = 23.3 --> ', classmates)
+# >>> classmates[1] = 'Sarah'
+# >>> classmates
+# ['Michael', 'Sarah', 'Tracy']
+# list里面的元素的数据类型也可以不同，比如：
+L = ['apple', 123, True]
+classmates[1] = L
+print('classmates  after set classmates[1] = L --> ', classmates)
+print('len(classmates)  after set classmates[1] = L --> ', len(classmates))
+# >>> L = ['Apple', 123, True]
+# list元素也可以是另一个list，比如：
+#
+# >>> s = ['python', 'java', ['asp', 'php'], 'scheme']
+# >>> len(s)
+# 4
+# 要注意s只有4个元素，其中s[2]又是一个list，如果拆开写就更容易理解了：
+#
+# >>> p = ['asp', 'php']
+# >>> s = ['python', 'java', p, 'scheme']
+# 要拿到'php'可以写p[1]或者s[2][1]，因此s可以看成是一个二维数组，类似的还有三维、四维……数组，不过很少用到。
+#
+# 如果一个list中一个元素也没有，就是一个空的list，它的长度为0：
+#
+# >>> L = []
+# >>> len(L)
+# 0
+# -----------------------tuple--------------------
+# 另一种有序列表叫元组：tuple。tuple和list非常类似，但是tuple一旦初始化就不能修改，比如同样是列出同学的名字：
+#
+# >>> classmates = ('Michael', 'Bob', 'Tracy')
+# 现在，classmates这个tuple不能变了，它也没有append()，insert()这样的方法。其他获取元素的方法和list是一样的，你可以正常地使用classmates[0]，classmates[-1]，但不能赋值成另外的元素。
+#
+# 不可变的tuple有什么意义？因为tuple不可变，所以代码更安全。如果可能，能用tuple代替list就尽量用tuple。
+#
+# tuple的陷阱：当你定义一个tuple时，在定义的时候，tuple的元素就必须被确定下来，比如：
+#
+# >>> t = (1, 2)
+# >>> t
+# (1, 2)
+# 如果要定义一个空的tuple，可以写成()：
+#
+# >>> t = ()
+# >>> t
+# ()
+# 但是，要定义一个只有1个元素的tuple，如果你这么定义：
+#
+# >>> t = (1)
+# >>> t
+# 1
+# 定义的不是tuple，是1这个数！这是因为括号()既可以表示tuple，又可以表示数学公式中的小括号，这就产生了歧义，因此，Python规定，这种情况下，按小括号进行计算，计算结果自然是1。
+#
+# 所以，只有1个元素的tuple定义时必须加一个逗号,，来消除歧义：
+#
+t = (1,)
+print('只有一个元素的tuple t = (1,) -->', t)
+# (1,)
+# Python在显示只有1个元素的tuple时，也会加一个逗号,，以免你误解成数学计算意义上的括号。
+#
+# 最后来看一个“可变的”tuple：
+#
+# >>> t = ('a', 'b', ['A', 'B'])
+# >>> t[2][0] = 'X'
+# >>> t[2][1] = 'Y'
+# >>> t
+# ('a', 'b', ['X', 'Y'])
+# 这个tuple定义的时候有3个元素，分别是'a'，'b'和一个list。不是说tuple一旦定义后就不可变了吗？怎么后来又变了？
+#
+# 别急，我们先看看定义的时候tuple包含的3个元素：
+#
+# tuple-0
+#
+# 当我们把list的元素'A'和'B'修改为'X'和'Y'后，tuple变为：
+#
+# tuple-1
+#
+# 表面上看，tuple的元素确实变了，但其实变的不是tuple的元素，而是list的元素。tuple一开始指向的list并没有改成别的list，
+# 所以，tuple所谓的“不变”是说，tuple的每个元素，指向永远不变。即指向'a'，就不能改成指向'b'，指向一个list，就不能改成指向其他对象，但指向的这个list本身是可变的！
+#
+# 理解了“指向不变”后，要创建一个内容也不变的tuple怎么做？那就必须保证tuple的每一个元素本身也不能变。
+#
 # 练习
+# 请用索引取出下面list的指定元素：
 #
-# 利用切片操作，实现一个trim()函数，去除字符串首尾的空格，注意不要调用str的strip()方法：
-def trim(string):
-    if string == '':
-        return ''
-    while string[:1] == ' ':
-        string = string[1:]
-    while string[-1:] == ' ':
-        string = string[:-1]
-    return string
-
-
-s = "  hello , weo  "
-print(trim(s))
-print(s.strip())
-print('after trim --%s--' % trim(s))
-print(s, 'after trim --' + trim(s) + '--')
-
-print('--------------迭代(Iteration)---------------')
-# 默认情况下，dict迭代的是key。如果要迭代value，可以用for value in d.values()，如果要同时迭代key和value，可以用for k, v in d.items()。
-dic = {'L0': 0, 'L1': 1, "L2": 2}
-for k, v in dic.items():
-    print('key=', k, ', value=', v)
-# 如何判断一个对象是可迭代对象呢？方法是通过collections模块的Iterable类型判断
-# from collections import Iterable
-# isinstance('abc', Iterable)
-
-# Python内置的enumerate函数可以把一个list变成索引-元素对
-for i, value in enumerate(['A', 'B', 'C']):
-    print("下标", i, "值", value)
-# 练习
+# # -*- coding: utf-8 -*-
 #
-# 请使用迭代查找一个list中最小和最大值，并返回一个tuple：
-list1 = {1, 23, 2, 23, 55, 2, 5, 15}
-
-
-def find_max_and_min(*args):
-    if len(args) == 0:
-        return None, None
-    max1 = args[0]
-    min1 = args[0]
-    for value1 in args:
-        if max1 < value1:
-            max1 = value1
-        if min1 > value1:
-            min1 = value1
-    return max1, min1
-
-
-print('max value = %d,and min value = %d' % find_max_and_min(*list1))
-print('--------------------列表生成式---------------------')
-# 举个例子，要生成list [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]可以用list(range(1, 11))：
-list2 = list(range(1, 11))
-print(list2)
-print([x * x for x in list2])
-print([m + n for m in 'ABC' for n in 'XYZ'])
-print([d for d in os.listdir('.')])
-L = ['Hello', 'World', 18, 'Apple', None]
-L2 = []
-[L2.append(s.lower()) for s in L if isinstance(s, str)]
-print([s for s in L if isinstance(s, str)])
-print(L2)
-
-print('--------------------生成器-----------------------')
-# 在Python中，这种一边循环一边计算的机制，称为生成器：generator。
-# 要创建一个generator，有很多种方法。第一种方法很简单，只要把一个列表生成式的[]改成()，就创建了一个generator：
-g = (s for s in L if isinstance(s, str))
-for n in g:
-    print(n)
-
-
-# 著名的斐波拉契数列（Fibonacci），除第一个和第二个数外，任意一个数都可由前两个数相加得到：
-# 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
-def fibonacci(max):
-    n, a, b = (0, 0, 1)
-    while n < max:
-        print(b)
-        a, b = b, a + b
-        n = n + 1
-
-    print('done')
-
-
-fibonacci(5)
-
-
-# 如果一个函数定义中包含yield关键字，那么这个函数就不再是一个普通函数，而是一个generator：
-def fib(max):
-    n, a, b = (0, 0, 1)
-    while n < max:
-        yield b
-        a, b = b, a + b
-        n = n + 1
-
-    print('done')
-
-
-f = fib(20)
-
-
-def triangles(lines):
-    n = 0
-    ret = [1]
-    while n <= lines:
-        yield ret
-        for j in range(1, len(ret)):
-            ret[j] = pre[j] + pre[j - 1]
-        ret.append(1)
-        pre = ret[:]
-        n = n + 1
-
-
+L1 = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
 #
-# def triangles():
-#     L = [1]
-#     while True:
-#         yield L
-#         L.append(0)
-#         L = [L[i - 1] + L[i] for i in range(len(L))]
-
-def triangle(lines):
-    pre = []
-    current = [1]
-    while lines > 1:
-        yield current
-        for m in range(1, len(current)):
-            current[m] = pre[m] + pre[m - 1]
-        current.append(1)
-        pre = current[:]
-
-
-tran = triangle(4)
-print(next(tran))
-print(next(tran))
-print(next(tran))
-print(next(tran))
+# # 打印Apple:
+print(L1[0][0])
+# # 打印Python:
+print(L1[1][1])
+# # 打印Lisa:
+print(L1[2][2])
+#
+#  Run
+# 小结
+# list和tuple是Python内置的有序集合，一个可变，一个不可变。根据需要来选择使用它们。
