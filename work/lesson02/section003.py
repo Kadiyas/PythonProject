edc7f76212dd931807a913bc86319678c273847a
kadiyas <1228164670@qq.com>
2018-08-22 17:46:52 +0800

    lesson001 数据类型 lesson002 字符串编码

--- work/lesson02/section003.py
+++ work/lesson02/section003.py
@@ -216,7 +216,7 @@ def calc(*numbers):
 # >>> calc()
 # 0
 # 如果已经有一个list或者tuple，要调用一个可变参数怎么办？可以这样做：
-#
+
 # >>> nums = [1, 2, 3]
 # >>> calc(nums[0], nums[1], nums[2])
 # 14
@@ -285,12 +285,12 @@ person('bob', 23, **{'p': 'que'})
 # >>> person('Jack', 24, city='Beijing', addr='Chaoyang', zipcode=123456)
 # 如果要限制关键字参数的名字，就可以用命名关键字参数，例如，只接收city和job作为关键字参数。这种方式定义的函数如下：
 #
-# def person(name, age, *, city, job):
-#     print(name, age, city, job)
+def person(name, age, *, city, job):
+    print(name, age, city, job)
 # 和关键字参数**kw不同，命名关键字参数需要一个特殊分隔符*，*后面的参数被视为命名关键字参数。
 #
 # 调用方式如下：
-#
+person('Jack', 24, city='Beijing', job='Engineer')
 # >>> person('Jack', 24, city='Beijing', job='Engineer')
 # Jack 24 Beijing Engineer
 # 如果函数定义中已经有了一个可变参数，后面跟着的命名关键字参数就不再需要一个特殊分隔符*了：
@@ -318,18 +318,21 @@ person('bob', 23, **{'p': 'que'})
 # def person(name, age, city, job):
 #     # 缺少 *，city和job被视为位置参数
 #     pass
-# 参数组合
-# 在Python中定义函数，可以用必选参数、默认参数、可变参数、关键字参数和命名关键字参数，这5种参数都可以组合使用。但是请注意，参数定义的顺序必须是：必选参数、默认参数、可变参数、命名关键字参数和关键字参数。
+# >>>>>>>>>>>>>>>>>>>>>>>>参数组合<<<<<<<<<<<<<<<<<<<<<<<
+# 在Python中定义函数，可以用必选参数、默认参数、可变参数、关键字参数和命名关键字参数，这5种参数都可以组合使用。
+# 但是请注意，参数定义的顺序必须是：必选参数、默认参数、可变参数、命名关键字参数和关键字参数。
 #
 # 比如定义一个函数，包含上述若干种参数：
 #
-# def f1(a, b, c=0, *args, **kw):
-#     print('a =', a, 'b =', b, 'c =', c, 'args =', args, 'kw =', kw)
-#
-# def f2(a, b, c=0, *, d, **kw):
-#     print('a =', a, 'b =', b, 'c =', c, 'd =', d, 'kw =', kw)
+def f1(a, b, c=0, *args, **kw):
+    print('a =', a, 'b =', b, 'c =', c, 'args =', args, 'kw =', kw)
+
+def f2(a, b, c=0, *, d, **kw):
+    print('a =', a, 'b =', b, 'c =', c, 'd =', d, 'kw =', kw)
 # 在函数调用的时候，Python解释器自动按照参数位置和参数名把对应的参数传进去。
 #
+f1(1, 2, [23, 12, 33], x=12)
+
 # >>> f1(1, 2)
 # a = 1 b = 2 c = 0 args = () kw = {}
 # >>> f1(1, 2, c=3)
